4524c42c7876e9634c698c5b0e4dbcaef548306f
cmurph11 <colinmurphy@wr-130-64-218-89.medford.tufts.edu>
2018-02-19 08:05:31 -0500

    new folder for D3 experimentation

---
+++ demo.js
@@ -0,0 +1,9 @@
+// averages calculation
+var avgs = d3.nest()
+	.key(function(d) {return d.continent;})
+	.sortkey(d3.ascending)
+	.rollup(function(d) {return{
+		GERD:d3.mean(function(g) {return +g.GERD;}),
+		growth:d3.mean(d,function(g) {return +g.growth})
+	}})
+	.entries(csv);
